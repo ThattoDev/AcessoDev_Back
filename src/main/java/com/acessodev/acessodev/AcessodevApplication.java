7cc21932a734f97d200047c5b0eac9ca8a176592
ThattoDev <thacito.pfilgueiras@gmail.com>
2022-11-23 22:12:01 -0300

    First commit

---
+++ src/main/java/com/acessodev/acessodev/AcessodevApplication.java
@@ -0,0 +1,13 @@
+package com.acessodev.acessodev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcessodevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcessodevApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/acessodev/acessodev/models/Usuario.java
@@ -0,0 +1,81 @@
+package com.acessodev.acessodev.models;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_usuario")
+public class Usuario implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+
+    private String nome;
+
+    private String email;
+
+    private String senha;
+
+    private String observacao;
+
+    private Date data_nascimento;
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getObservacao() {
+        return observacao;
+    }
+
+    public void setObservacao(String observacao) {
+        this.observacao = observacao;
+    }
+
+    public Date getData_nascimento() {
+        return data_nascimento;
+    }
+
+    public void setData_nascimento(Date data_nascimento) {
+        this.data_nascimento = data_nascimento;
+    }
+
+}
